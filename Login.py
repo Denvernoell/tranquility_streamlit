15ddb21878e7688c55507df6af68d4c8db3e31b4
Denver Noell <dnoell@ppeng.com>
2023-01-18 20:14:50 -0800

    removed external requirements

--- Login.py
+++ Login.py
@@ -2,9 +2,8 @@ import streamlit as st
 import pandas as pd
 import supabase
 
-# from dashboard_shared import Table,Components
-from streamlit_data_management.dashboard import Table,Components
-
+from dashboard_shared import Table,Components
+# from streamlit_data_management.dashboard import Table,Components
 
 C = Components("Tranquillity")
 C.header()
