b5f51854e989f3b31c9c7a697e708619ec37aa79
Denver Noell <dnoell@ppeng.com>
2023-01-18 19:58:58 -0800

    Trial with streamlit_data_management

--- pages/3_Subsidence.py
+++ pages/3_Subsidence.py
@@ -14,7 +14,6 @@ import arrow
 
 import geopandas as gpd
 
-
 from dashboard_shared import Table,Components,export_df
 
 C = Components("Tranquillity")
@@ -42,6 +41,8 @@ def get_table(table_name):
 	return pd.DataFrame(st.session_state['client'].table(table_name).select('*').execute().data)
 
 table_name = 'TID_subsidence_points'
+
+
 if st.session_state['Logged In']:
 	def plot_positions(df,points_to_use):
 		df = df.loc[df['point_id'].isin(points_to_use)]	
@@ -88,7 +89,10 @@ if st.session_state['Logged In']:
 		# 	)#.to_crs("EPSG:4326")
 		# 	# ! figure out why this doesnt work
 		
-		boundaries = get_table('TID_gis_boundaries')
+		table_name = 'TID_gis_boundaries'
+		# boundaries = get_table(table_name)
+		boundaries = Table(table_name).df
+
 		boundaries_df = boundaries.loc[boundaries['file_name'].isin(['Tranquillity Irrigation District',"Fresno Slough Water District"])]
 		from shapely import wkb,wkt
 		boundaries_gdf = gpd.GeoDataFrame(boundaries_df,geometry=boundaries_df['geometry'].apply(wkt.loads),crs="EPSG:4326")
@@ -106,19 +110,20 @@ if st.session_state['Logged In']:
 		return points_to_use
 
 	# positions = st.session_state.dfs['TID_subsidence_points']
-	positions = get_table('TID_subsidence_points')
+	table_name = 'TID_subsidence_points'
+	# positions = get_table(table_name)
+	positions = Table(table_name).df
+
 	points = [i for i in positions['point_id'].unique()]
 	points_to_use = st.multiselect("Points",points,default=points)
 	if points_to_use:
 		plot_positions(positions,points_to_use)
 		# elevations = st.session_state.dfs['TID_subsidence_elevations']
-		elevations = get_table('TID_subsidence_elevations')
+		# elevations = get_table('TID_subsidence_elevations')
+		elevations = Table('TID_subsidence_elevations').df
 		gse(elevations,points_to_use)
 
 else:
 	st.markdown('Not logged in')
 
-
-
-
-
+C.footer()
